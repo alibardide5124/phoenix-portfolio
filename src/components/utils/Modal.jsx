6979b7ce61f0d395bed08db115aac3904c7aa310
Ali Bardide <alibardide5124@gmail.com>
2025-04-05 12:14:37 +0330

    Improve project modal

--- src/components/utils/Modal.jsx
+++ src/components/utils/Modal.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { clamp, motion } from "framer-motion";
 import GitHubIcon from "./GitHubIcon";
+import CloseIcon from "./CloseIcon";
 
 const BackDrop = ({ children, onClick }) => {
   return (
@@ -26,11 +27,22 @@ const Modal = ({ project, handleClose }) => {
           exit={{ y: "20%", opacity: 0 }}
           onClick={(e) => e.stopPropagation()}
           style={{ width: clamp("50%", "600px", "90%") }}
-          className="flex flex-col mb-10 mt-10 mx-6 rounded-md bg-foreground "
+          className="relative flex flex-col mb-10 mt-10 mx-6 rounded-md bg-foreground "
         >
           <img className="w-full aspect-[16/9]" src={project.imageUrl} />
+          <div 
+          onClick={handleClose}
+        className="absolute start-0 top-0 m-3 group transition ease-in-out rounded-md items-center justify-center p-1 border-2 border-white hover:bg-white cursor-pointer">
+            <CloseIcon className="transition ease-in-out fill-copy size-5 group-hover:fill-black" />
+          </div>
           <h1 className="text-lg px-4 my-2">{project.title}</h1>
-          <p className="text-xs px-4 text-copy-lighter">{project.skills}</p>
+          <div className="flex flex-wrap px-2  gap-2 overflow-visible">
+              {project.skills.map((item, index) => (
+                <div key={index} className="rounded-md p-2 bg-border">
+                  <p className="text-xs">{item}</p>
+                </div>
+              ))}
+            </div>
           <p className="text-xs px-4 mt-4" style={{ lineHeight: 2 }}>
             {project.description}
           </p>
